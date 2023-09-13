2b2f201b0ee45f5bfa41e2c8141fa5d22fcc6dc7
alphal <xiaodao_92@yeah.net>
2023-09-13 14:52:47 +0800

    feat(wujie-main): add global app(message, modal, notification)

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,4 +1,5 @@
 import LayoutErrorBoundary from '@/layouts/errorBoundart';
+import { App, ConfigProvider } from 'antd';
 import React from 'react';
 import { Outlet } from 'umi';
 // import styles from "./index.module.less";
@@ -9,14 +10,20 @@ export interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = (props: LayoutProps) => {
+  const { message, modal, notification } = App.useApp();
+
   return (
     <React.Fragment>
-      {/*LayoutComp*/}
-      {/*{props.children}*/}
-      {/*props: {JSON.stringify(props)}<br/>*/}
-
       <LayoutErrorBoundary>
-        <Outlet />
+        <ConfigProvider direction="rtl">
+          <App
+            className="app"
+            message={{ maxCount: 1 }}
+            notification={{ placement: 'bottomLeft' }}
+          >
+            <Outlet />
+          </App>
+        </ConfigProvider>
       </LayoutErrorBoundary>
     </React.Fragment>
   );
